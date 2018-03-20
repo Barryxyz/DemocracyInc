8756eebf7a5d5e8802b20dc14115267eb489800f
pzr5dy <35753330+pzr5dy@users.noreply.github.com>
2018-03-19 22:54:10 -0400

    Update views.py

--- vote/views.py
+++ vote/views.py
@@ -18,18 +18,3 @@ def login(request):
 
 def checkin(request):
     return render(request, 'vote/checkin.html', {})
-	
-def ballot(request):
-    # if this is a POST request we need to process the form data
-    if request.method == 'POST':
-        # create a form instance and populate it with data from the request:
-        form = VoteForm(request.POST)
-        # check whether it's valid:
-        if form.is_valid():
-            # process the data in form.cleaned_data as required
-            # ...
-            # redirect to a new URL:
-            return HttpResponseRedirect('/home/')
-    else:
-        form = VoteForm()
-    return render(request, 'vote/vote.html', {'form': form})

4685afea8ffc2eced6b952a81295c887b88ac719
Unknown <pzr5dy@virginia.edu>
2018-03-28 12:33:27 -0400

    staging changes

--- vote/views.py
+++ vote/views.py
@@ -4,6 +4,7 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 from django.views.generic import TemplateView
 from django.http import HttpResponse
+from .forms import VoteForm
 
 
 # Create your views here.

e9023aff88ca5a8776cd591bf02a9cedd0255472
sanjanamendu <sm7gc@virginia.edu>
2018-04-12 18:38:07 -0400

    set up database

--- vote/urls.py
+++ vote/urls.py
@@ -21,5 +21,5 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^', include('web.urls', namespace='web')),
+    url(r'^', include('web.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)

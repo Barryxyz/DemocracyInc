3a4728398b922575ccec0b87cca62db670d98faf
sanjanamendu <sm7gc@virginia.edu>
2018-04-15 16:06:47 -0400

    successfully reimplemented login page and credentials

--- vote/views.py
+++ vote/views.py
@@ -2,9 +2,10 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render, redirect
-from django.contrib.auth.decorators import login_required
 from django.urls import reverse
-from .forms import VoteForm,VoteIdCheckForm,RegisteredForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from .forms import VoteForm,VoteIdCheckForm,RegisteredForm,LoginForm
 from .models import Voter
 import random
 
@@ -14,7 +15,27 @@ def home(request):
     return render(request, 'base.html', {})
 
 def login(request):
-    return render(request, 'login.html', {})
+    if request.method == 'POST':
+        # create a form instance and populate it with data from the request:
+        form = LoginForm(request.POST)
+        # check whether it's valid:
+        if form.is_valid():
+            valid_pollworker = User.objects.filter(
+                username=form.cleaned_data['username'],
+                password=form.cleaned_data['password']
+            ).exists()
+
+            if valid_pollworker:
+                return redirect(reverse('vote_id_check'))
+            else:
+                return redirect(reverse('home'))
+            # process the data in form.cleaned_data as required
+            # redirect to a new URL:
+
+    # if a GET (or any other method) we'll create a blank form
+    else:
+        form = LoginForm()
+    return render(request, './registration/login.html', {'form': form})
 
 def logout_page(request):
     return render(request, 'registration/logout_success.html', {})

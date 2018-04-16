e59109985fcca7db0db0a93afb34ca144eb050ec
Unknown <pzr5dy@virginia.edu>
2018-04-16 12:26:06 -0400

    progress on election results chart

--- vote/views.py
+++ vote/views.py
@@ -9,17 +9,21 @@ from django.db.models import Count
 
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
-from .forms import VoteForm,VoteIdCheckForm,RegisteredForm,LoginForm
+from .forms import VoteForm, VoteIdCheckForm, RegisteredForm, LoginForm
 
 import random
-from graphos.sources.simple import ModelDataSource
-from graphos.renderers.gchart import BarChart
+import json
+
+from graphos.sources.model import SimpleDataSource
+from graphos.renderers import morris
+
 
 
 # Create your views here.
 
 def home(request):
     return render(request, 'base.html', {})
+
 
 def login(request):
     if request.method == 'POST':
@@ -44,16 +48,20 @@ def login(request):
         form = LoginForm()
     return render(request, './registration/login.html', {'form': form})
 
+
 def logout_page(request):
     return render(request, 'registration/logout_success.html', {})
 
-def reset(request):
-	return render(request, 'registration/password_reset_form.html', {})
 
-@login_required	
+def reset(request):
+    return render(request, 'registration/password_reset_form.html', {})
+
+
+@login_required
 def view_voters(request):
-	query_results = Voter.objects.all()
-	return render(request, 'view_voters.html', {'query_results': query_results})
+    query_results = Voter.objects.all()
+    return render(request, 'view_voters.html', {'query_results': query_results})
+
 
 @login_required
 def checkin(request):
@@ -80,7 +88,8 @@ def checkin(request):
                 locality = task.locality
                 task.confirmation = key
                 task.save()
-                return render(request, 'booth_assignment.html', {'booth': key, 'full_name': full_name, 'locality': locality})
+                return render(request, 'booth_assignment.html',
+                              {'booth': key, 'full_name': full_name, 'locality': locality})
 
             else:
                 return render(request, 'notregistered.html', {})
@@ -88,6 +97,7 @@ def checkin(request):
     else:
         form = RegisteredForm()
     return render(request, 'checkin.html', {'form': form})
+
 
 def vote(request):
     if request.method == 'POST':
@@ -107,6 +117,7 @@ def vote(request):
         form = VoteForm()
     return render(request, 'vote.html', {'form': form})
 
+
 def vote_id_check(request):
     if request.method == 'POST':
         # create a form instance and populate it with data from the request:
@@ -119,7 +130,7 @@ def vote_id_check(request):
                 request.session['input_key'] = input_key
                 return redirect(reverse('vote'))
             else:
-                return render(request, 'vote_id_check.html', {'form': form}) # need an error page?
+                return render(request, 'vote_id_check.html', {'form': form})  # need an error page?
         else:
             return redirect(reverse('home'))
 
@@ -128,28 +139,48 @@ def vote_id_check(request):
         form = VoteIdCheckForm()
     return render(request, 'vote_id_check.html', {'form': form})
 
+
 def generator():
     seq = "ABCDFGHJIKLMNOPQRSTUVWXYZ1234567890"
     key = ''
 
     for i in range(6):
-        key+=(''.join(''.join(random.choice(seq))))
+        key += (''.join(''.join(random.choice(seq))))
     return key
 
-def results():
-    queryset = VoteRecord.objects.all()
-    data_source = ModelDataSource(queryset,
-                                  fields=['president', 'governor','lieutenant_Governor','attorney_General','delegate','commonwealth_Attorney','sheriff','treasurer'])
-    chart = graphos.renderers.flot.BarChart(data_source, options={'title': "Election Results", 'xaxis': {'mode': "Count"}})
-    return django.shortcuts.render(request,template_name='results_display.html', context= {'chart': chart})
+
+# def results():
+#     queryset = VoteRecord.objects.all()
+#     data_source = ModelDataSource(queryset,
+#                                   fields=['president', 'governor','lieutenant_Governor','attorney_General','delegate','commonwealth_Attorney','sheriff','treasurer'])
+#     chart = graphos.renderers.flot.BarChart(data_source, options={'title': "Election Results", 'xaxis': {'mode': "Count"}})
+#     return django.shortcuts.render(request,template_name='results_display.html', context= {'chart': chart})
+#
+#
+#
+def vote_count(request):
+    # VoteRecord.objects.filter()
+    # VoteRecord.objects.filter(president="Hillary Clinton")
+    # presidents = VoteRecord.objects.annotate(Count('president'))
+    prez_count = VoteRecord.objects.filter(president='Gary Johnson').count()
+    prez_count2 = VoteRecord.objects.filter(president='Donald Trump').count()
+    # data = [
+    #     ['President','Count'],
+    #     ['Gary Johnson', prez_count],
+    #     ['Donald Trump', prez_count2]
+    # ]
+    data = [
+        ['Year', 'Sales'],
+        [2004, 1000],
+        [2005, 1170],
+        [2006, 660],
+        [2007, 1030]
+    ]
+    # # queryset = VoteRecord.objects.all()
+    # # data_source = ModelDataSource(queryset, fields=['president', 'count'])  # x-axis = presidet, y-axis = count
+    # # chart = gchart.BarChart(data_source, options={'title': "Election Results", 'xaxis': {'mode': "Count"}})
+    data_source = SimpleDataSource(data=data)
+    chart = morris.LineChart(data_source, options={'title': "Line Chart"},html_id='gchart_div')
+    return render(request,'vote_count.html', {'chart': chart})
 
 
-def vote_results(request):
-    if request.method == 'POST':
-        #VoteRecord.objects.filter()
-        #VoteRecord.objects.filter(president="Hillary Clinton")
-        presidents = VoteRecord.objects.annotate(Count('president'))
-
-        print(presidents)
-
-    return render(request, 'vote_count.html', {})

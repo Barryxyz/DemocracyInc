c7fbaab5be76f13936f7156bd696a874cac482d8
Unknown <mvw5mf@virginia.edu>
2018-05-01 20:53:37 -0400

    fixed admin page

--- vote/admin.py
+++ vote/admin.py
@@ -4,16 +4,19 @@ from __future__ import unicode_literals
 from django.contrib import admin
 
 # Register your models here.
-from .models import Voter, PollWorker, VoteCount, VoteRecord, Election
+from .models import Election, Position, Candidate, VoteRecord, VoteCount, Voter
+
+class election_types(admin.ModelAdmin):
+        model = Election
+
+class positions(admin.ModelAdmin):
+        model = Position
+
+class candidate_list(admin.ModelAdmin):
+        model = Candidate
 
 class registered_voters(admin.ModelAdmin):
         model = Voter
-
-class pollworkers(admin.ModelAdmin):
-        model = PollWorker
-
-class elections(admin.ModelAdmin):
-        model = Election
 
 class countvotes(admin.ModelAdmin):
         model = VoteCount
@@ -23,8 +26,9 @@ class voterecord(admin.ModelAdmin):
 
 
 # instantiating all the pages so that they exist in the admin page
+admin.site.register(Election, election_types)
+admin.site.register(Position, positions)
+admin.site.register(Candidate, candidate_list)
 admin.site.register(Voter, registered_voters)
-admin.site.register(PollWorker, pollworkers)
-admin.site.register(Election, elections)
 admin.site.register(VoteCount, countvotes)
 admin.site.register(VoteRecord, voterecord)

c1f6442fd618ff6ba9ad7b124daa1ae7efd59c90
sanjanamendu <sm7gc@virginia.edu>
2018-05-01 20:21:13 -0400

    fixed everything so we can support multiple elections w/o hardcoding

--- vote/serializers.py
+++ vote/serializers.py
@@ -2,21 +2,13 @@ from vote import models
 from rest_framework import serializers
 
 # used to access the database for external api to return types of elections available
-class electionSerializer(serializers.HyperlinkedModelSerializer):
-    id = serializers.CharField(source='election_id')
+class ElectionSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = models.Election
-        fields = ('id', 'type',)
+        fields = ('election_id', 'type',)
 
-# used to access the database for external api to return ballots of the primary election
-class voteSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = models.VoteRecord
-        fields = "__all__"
-
-# used to access the database for external api to return the election result statistics
-class CountSerializer(serializers.HyperlinkedModelSerializer):
+# used to access the database for external api to return ballots of the active election
+class VoteCountSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = models.VoteCount
-        fields = ('name', 'position', 'count',)
-
+        fields = ('position', 'candidate','count')
